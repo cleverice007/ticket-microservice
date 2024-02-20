4ebc740b67d36d4fd5ef9db724c16ed0ece89d86
cleverice007 <cleverice90128@gmail.com>
2024-02-20 16:09:17 +0800

    create do-request hook

---
+++ client/hooks/use-request.js
@@ -0,0 +1,32 @@
+import axios from 'axios';
+import { useState } from 'react';
+
+export default ({ url, method, body, onSuccess }) => {
+  const [errors, setErrors] = useState(null);
+
+  const doRequest = async () => {
+    try {
+      setErrors(null);
+      const response = await axios[method](url, body);
+
+      if (onSuccess) {
+        onSuccess(response.data);
+      }
+
+      return response.data;
+    } catch (err) {
+      setErrors(
+        <div className="bg-red-500 text-white font-bold rounded-t px-4 py-2">
+          <h4 className="text-lg">Ooops....</h4>
+          <ul className="list-disc pl-5">
+            {err.response.data.errors.map((err) => (
+              <li key={err.message}>{err.message}</li>
+            ))}
+          </ul>
+        </div>
+      );
+    }
+  };
+
+  return { doRequest, errors };
+};
