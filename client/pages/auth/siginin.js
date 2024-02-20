776198852b8e1c842103cd3142e9123fc525f460
cleverice007 <cleverice90128@gmail.com>
2024-02-20 15:57:01 +0800

    create signin page

---
+++ client/pages/auth/siginin.js
@@ -0,0 +1,50 @@
+import { useState, useEffect } from 'react';
+import Router from 'next/router';
+import useRequest from '../../hooks/use-request';
+
+export default () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const { doRequest, errors } = useRequest({
+    url: '/api/users/signin',
+    method: 'post',
+    body: {
+      email,
+      password
+    },
+    onSuccess: () => Router.push('/')
+  });
+
+  const onSubmit = async event => {
+    event.preventDefault();
+
+    await doRequest();
+  };
+
+return (
+    <form onSubmit={onSubmit} className="space-y-4">
+      <h1 className="text-xl font-bold">Sign In</h1>
+      <div className="form-group">
+        <label className="block mb-2 text-sm font-medium text-gray-900">Email Address</label>
+        <input
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          placeholder="you@example.com"
+        />
+      </div>
+      <div className="form-group">
+        <label className="block mb-2 text-sm font-medium text-gray-900">Password</label>
+        <input
+          value={password}
+          onChange={e => setPassword(e.target.value)}
+          type="password"
+          className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5"
+          placeholder="••••••••"
+        />
+      </div>
+      {errors}
+      <button className="text-white bg-blue-500 hover:bg-blue-700 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center">Sign In</button>
+    </form>
+  );
+};
