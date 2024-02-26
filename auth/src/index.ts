e96bbe7c004558466ec88ccbad1826a3d29b7bb1
cleverice007 <cleverice90128@gmail.com>
2024-02-26 14:52:47 +0800

    debug

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,34 +1,22 @@
-import express from 'express';
-import 'express-async-errors';
-import { json } from 'body-parser';
-import cookieSession from 'cookie-session';
+import mongoose from 'mongoose';
 
-import { currentUserRouter } from './routes/current-user';
-import { signinRouter } from './routes/signin';
-import { signoutRouter } from './routes/signout';
-import { signupRouter } from './routes/signup';
-import { errorHandler } from '../../common/src/middlewares/error-handler';
-import { NotFoundError } from './errors/not-found-error';
+import { app } from './app';
 
-const app = express();
-app.set('trust proxy', true);
-app.use(json());
-app.use(
-  cookieSession({
-    signed: false,
-    secure: process.env.NODE_ENV !== 'test'
-  })
-);
+const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
 
-app.use(currentUserRouter);
-app.use(signinRouter);
-app.use(signoutRouter);
-app.use(signupRouter);
+  try {
+    mongoose.connect('mongodb+srv://cleverice90128:cleverice007@cluster0.ckooh1c.mongodb.net/ticket-microservice?retryWrites=true&w=majority');
+    console.log('Connected to MongoDb');
+  } catch (err) {
+    console.error(err);
+  }
 
-app.all('*', async (req, res) => {
-  throw new NotFoundError();
-});
+  app.listen(3000, () => {
+    console.log('Listening on port 3000!!!!!!!!');
+  });
+};
 
-app.use(errorHandler);
-
-export { app };
+start();
