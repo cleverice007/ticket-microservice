71818f0d0b3896795cd43bba9f98f714b4fcb741
cleverice007 <cleverice90128@gmail.com>
2024-03-19 16:11:57 +0800

    create nats deployment

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -4,7 +4,7 @@ import { TicketCreatedPublisher } from './events/ticket-created-publisher';
 console.clear();
 
 const stan = nats.connect('ticketing', 'abc', {
-  url: 'http://localhost:4222',
+  url: 'nats://localhost:4222',
 });
 
 stan.on('connect', async () => {
